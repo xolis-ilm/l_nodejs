915e2a93b6f2ce7006d037261a9fcd147fe16a2a
xolisilm <xolisilm2@gmail.com>
2023-02-25 02:11:22 +0500

    15 html bilan ishlash

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
-const http = require('http');
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 const server = http.createServer((req, res) => {
-    console.log(req.method)
-    if (req.method === 'GET') {
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.end('<h2>Salom Dunyo</h2> <form action="/" method="post">' +
-            '<input type="text" name="body">' +
-            '<button type="submit">submit</button></form>')
-    } else if (req.method === 'POST') {
-        const body = []
-        req.on('data', (data) => {
-            body.push(Buffer.from(data))
-            res.writeHead(200, {'Content-Type': 'text/html charset="utf-8'})
+	if (req.method === 'GET') {
+		res.writeHead(200, { 'Content-Type': 'text/html' })
 
-        })
-        req.on('end', () => {
-            const mess = body.toString().split('=')[1]
-            res.end('Email succesfully added' + ' ' + mess)
-        })
-    }
+		if (req.url === '/') {
+			fs.readFile(path.join(__dirname, 'templates', 'index.html'), 'utf8', (err, content) => {
+				if (err) throw err
+				res.end(content)
+			})
+		} else if (req.url === '/about') {
+			fs.readFile(path.join(__dirname, 'templates', 'about.html'), 'utf8', (err, content) => {
+				if (err) throw err
+				res.end(content)
+			})
+		}else if (req.url === '/contact') {
+			fs.readFile(path.join(__dirname, 'templates', 'contact.html'), 'utf8', (err, content) => {
+				if (err) throw err
+				res.end(content)
+			})
+		}
+
+	} else if (req.method === 'POST') {
+		const body = []
+		res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+
+		req.on('data', data => {
+			body.push(Buffer.from(data))
+		})
+
+		req.on('end', () => {
+			const message = body.toString().split('=')[1]
+
+			res.end(`Name successfully added: ${message}`)
+		})
+	}
 })
 
-server.listen(5779, () => {
-    console.log('Server is running on port 3000');
-});
+const port = 8080
+
+server.listen(port, () => {
+	console.log(`Server has been started on port: ${port}`)
+})
